599a42911b9dee5c393cf1c450332dd27a91889d
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-07 22:00:21 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(num)
+  if num % 3 == 0
+    puts "Fizz"
+  elsif num % 5 == 0
+     puts "Buzz"
+   elsif num % 3 && 5 == 0
+     puts "FizzBuzz"
+  end
+end
