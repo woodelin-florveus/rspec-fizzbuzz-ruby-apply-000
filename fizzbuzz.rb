0156af14cc7b3b9b4d0d62aa0ffeec6ae7509d14
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-07 22:14:47 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(num)
-  if num % 3 == 0
-    "Fizz"
-  elsif num % 5 == 0
+  if num % 3 && 5 == 0
+    "FizzBuzz"
+  elsif num % 3 == 0
+      "Fizz"
+    elsif num %  5 == 0
       "Buzz"
-    elsif num % 3 && 5 == 0
-      "FizzBuzz"
   end
 end
